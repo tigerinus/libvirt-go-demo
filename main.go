73ce4e2646aa28a7f88ac1e710985ed6c3df18fa
Tiger Wang <tigerwang@outlook.com>
2024-01-24 17:21:08 +0800

    wip

--- main.go
+++ main.go
@@ -15,21 +15,34 @@ func main() {
 	}
 	defer conn.Close()
 
-	domains, err := conn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE | libvirt.CONNECT_LIST_DOMAINS_INACTIVE)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for _, domain := range domains {
-		domainName, err := domain.GetName()
+	for i := 0; i < 2; i++ {
+		domains, err := conn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE | libvirt.CONNECT_LIST_DOMAINS_INACTIVE)
 		if err != nil {
-			_ = fmt.Errorf("error when trying to get the name of a domain: %s", err.Error())
+			panic(err.Error())
 		}
 
-		fmt.Println(domainName)
+		for _, domain := range domains {
+			func(domain libvirt.Domain) {
+				defer func() {
+					if err := domain.Free(); err != nil {
+						_ = fmt.Errorf("error when trying to free a domain: %s", err.Error())
+					}
+				}()
 
-		if err := domain.Free(); err != nil {
-			_ = fmt.Errorf("error when trying to free a domain: %s", err.Error())
+				domainName, err := domain.GetName()
+				if err != nil {
+					_ = fmt.Errorf("error when trying to get the name of a domain: %s", err.Error())
+				}
+
+				state, _, err := domain.GetState()
+				if err != nil {
+					_ = fmt.Errorf("error when trying to get the state of a domain: %s", err.Error())
+				}
+
+				_, _ = fmt.Printf("%s (%d)\n", domainName, state)
+			}(domain)
 		}
 	}
+
+	fmt.Println("Done.")
 }
