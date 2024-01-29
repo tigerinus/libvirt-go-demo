276fdee96259c6ac115101a3af90c0c206a787f6
Tiger Wang <tigerwang@outlook.com>
2024-01-29 18:17:04 +0800

    wip

--- installermedia.go
+++ installermedia.go
@@ -1,6 +1,34 @@
 package main
 
 type InstallerMedia struct {
-	Label string
-	OS    *OSInfo
+	OS       *OSInfo
+	Label    string
+	DiskFile string
+
+	hostname string
+}
+
+func (im *InstallerMedia) PrepareForInstallation(vmName string) {
+	im.PrepareToContinueInstallation(vmName)
+
+	// TODO
+}
+
+func (im *InstallerMedia) PrepareToContinueInstallation(vmName string) {
+	im.hostname = ReplaceRegex(vmName, "[{|}~[\\]^':; <=>?@!\"#$%`()+/.,*&]", "")
+
+	unattended := "unattended.iso"
+
+	path := im.GetUserUnattended(&unattended)
+
+	im.DiskFile = path
+}
+
+func (im *InstallerMedia) GetUserUnattended(suffix *string) string {
+	filename := im.hostname
+	if suffix != nil {
+		filename += "-" + *suffix
+	}
+
+	return GetUserPkgCache(filename)
 }
